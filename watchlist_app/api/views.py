856437f61a40d6fdbcd9a427c684d355f45a7748
Mohammed Dilshad <dilshadt101@gmail.com>
2023-06-30 22:38:21 +0530

    review user and validation

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -54,10 +55,17 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
 
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+
         serializer.save(watchlist=watchlist)
 
 
