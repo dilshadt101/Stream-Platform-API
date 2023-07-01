8c79784cffdc616495125d237f81de6ada2249e3
Mohammed Dilshad <dilshadt101@gmail.com>
2023-07-01 18:38:23 +0530

    average rating and number of reviews

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -69,10 +69,19 @@ class ReviewCreate(generics.CreateAPIView):
 
         review_user = self.request.user
         review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie!")
 
-        serializer.save(watchlist=watchlist)
+        if watchlist.number_rating == 0:
+            watchlist.avg_rating = serializer.validated_data['rating']
+        else:
+            watchlist.avg_rating = (watchlist.avg_rating + serializer.validated_data['rating'])/2
+
+        watchlist.number_rating += 1
+        watchlist.save()
+
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
