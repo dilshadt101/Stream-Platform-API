847a7ecce1eeaa29f12ff332292e591f1c300209
Mohammed Dilshad <dilshadt101@gmail.com>
2023-07-04 06:29:18 +0530

    Login and logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models
+
+
+@api_view(['POST'])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -8,8 +20,19 @@ def registration_view(request):
 
     if request.method == 'POST':
         ser = RegistrationSerializer(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data)
 
-        return Response(ser.errors)
+        data = {}
+
+        if ser.is_valid():
+            account = ser.save()
+            data['response'] = "Registration successful"
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = ser.errors
+
+        return Response(data)
+
