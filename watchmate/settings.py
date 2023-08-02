b82bdb07a589fbd424e46b60951417b3885876c3
Mohammed Dilshad <dilshadt101@gmail.com>
2023-08-02 21:21:57 +0530

    Browsable api update

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -144,7 +144,10 @@ REST_FRAMEWORK = {
         'review-detail': '2/day'
     },
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 100
+    # 'PAGE_SIZE': 100,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 
